edd51b27407b6e06c63e64df6b80ae63da909d9c
Manas Ranjan Jena <mranjanjena253@gmail.com>
2026-06-23 09:38:02 +0530

    Added arboris.go

--- go_server/webhook/queue/tracker.go
+++ go_server/webhook/queue/tracker.go
@@ -1,5 +1,28 @@
 package queue
 
-type jobEntry struct {
-	jobError string
+type Job struct {
+	InstallationID string
+	Owner          string
+	RepoName       string
+	PRNumber       int
+	CommitID       string
+}
+
+type jobQueue struct {
+	queue chan *Job
+}
+
+func NewJobQueue(bufferSize int) *jobQueue {
+	return &jobQueue{
+		queue: make(chan *Job, bufferSize),
+	}
+}
+
+func (jobBuffer *jobQueue) JobEnqueue(job *Job) bool {
+	select {
+	case jobBuffer.queue <- job:
+		return true
+	default:
+		return false
+	}
 }
